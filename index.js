bff052adea5662d8e2e828975fbd453a04adcdde
“Madeline <mmoritz0808@gmail.com>
2020-11-24 18:40:04 -0600

    Hopefully good

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const Mustache = require("mustache");
+const fetch = require("node-fetch");
 const fs = require("fs");
+const puppeteerService = require("./services/puppeteer.service");
+
 const MUSTACHE_MAIN_DIR = "./main.mustache";
 
-// DATA to be provided to M.
-
 let DATA = {
-  name: "Madeline",
-  date: new Date().toLocaleDateString("en-US", {
+  refresh_date: new Date().toLocaleDateString("en-US", {
     weekday: "long",
     month: "long",
     day: "numeric",
@@ -16,16 +17,64 @@ let DATA = {
   }),
 };
 
-// Ask Mustache to render our file with the data we provided.
+async function setWeatherInformation() {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=austin&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then((r) => r.json())
+    .then((r) => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+    });
+}
 
-// Then use that data to generate a ReadMe.md file
+async function setInstagramPosts() {
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount(
+    "hachigo8",
+    3
+  );
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
+}
 
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
-    const output = Mustache.render(data.toString());
+    const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync("README.md", output);
   });
 }
 
-generateReadMe();
+async function action() {
+  /**
+   * Fetch Weather
+   */
+  await setWeatherInformation();
+
+  /**
+   * Get pictures
+   */
+  await setInstagramPosts();
+
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+
+  /**
+   * Fermeture de la boutique 👋
+   */
+  await puppeteerService.close();
+}
+
+action();
